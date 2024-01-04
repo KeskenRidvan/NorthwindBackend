b0be35b43cdb79096e03159b804244444c3f411f
Ridvan Kesken <kesken.dev@gmail.com>
2024-01-04 23:48:42 +0300

    Extension was made in Claim object.

--- Northwind.Core/Utilities/Security/JWT/Concrete/JwtHelper.cs
+++ Northwind.Core/Utilities/Security/JWT/Concrete/JwtHelper.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 using Northwind.Core.Entities.Concrete;
+using Northwind.Core.Extensions;
 using Northwind.Core.Utilities.Security.Encyption;
 using Northwind.Core.Utilities.Security.JWT.Abstract;
 using System.IdentityModel.Tokens.Jwt;
@@ -27,28 +28,29 @@ public class JwtHelper : ITokenHelper
 		var securityKey = SecurityKeyHelper.CreateSecurityKey(_tokenOptions.SecurityKey);
 		var signingCredentials = SigningCredentialsHelper.CreateSigningCredentials(securityKey);
 	}
-}
 
-public JwtSecurityToken CreateJwtSecurityToken(TokenOptions tokenOptions, User user, SigningCredentials signingCredentials, List<OperationClaim> operationClaims)
-{
-	JwtSecurityToken jwt = new(
-					 issuer: tokenOptions.Issuer,
-					 audience: tokenOptions.Audience,
-					 expires: _accessTokenExpiration,
-					 notBefore: DateTime.Now,
-					 claims: SetClaims(user, operationClaims),
-					 signingCredentials: signingCredentials);
+	public JwtSecurityToken CreateJwtSecurityToken(TokenOptions tokenOptions, User user, SigningCredentials signingCredentials, List<OperationClaim> operationClaims)
+	{
+		JwtSecurityToken jwt = new(
+						 issuer: tokenOptions.Issuer,
+						 audience: tokenOptions.Audience,
+						 expires: _accessTokenExpiration,
+						 notBefore: DateTime.Now,
+						 claims: SetClaims(user, operationClaims),
+						 signingCredentials: signingCredentials);
 
-	return jwt;
-}
+		return jwt;
+	}
 
-private IEnumerable<Claim> SetClaims(User user, List<OperationClaim> operationClaims)
-{
-	var claims = new List<Claim>();
-	claims.AddNameIdentifier(user.Id.ToString());
-	claims.AddEmail(user.Email);
-	claims.AddName($"{user.FirstName} {user.LastName}");
-	claims.AddRoles(operationClaims.Select(r => r.Name).ToArray());
+	private IEnumerable<Claim> SetClaims(User user, List<OperationClaim> operationClaims)
+	{
+		var claims = new List<Claim>();
+		claims.AddNameIdentifier(user.Id.ToString());
+		claims.AddEmail(user.Email);
+		claims.AddName($"{user.FirstName} {user.LastName}");
+		claims.AddRoles(operationClaims.Select(r => r.Name).ToArray());
 
-	return claims;
+		return claims;
+	}
+
 }
