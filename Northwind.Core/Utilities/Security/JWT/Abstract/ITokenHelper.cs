fe20e70a5a6b299aa4447ef70454a34544d47f93
Ridvan Kesken <kesken.dev@gmail.com>
2024-01-04 22:45:15 +0300

    Jwt infrastructure installed.

---
+++ Northwind.Core/Utilities/Security/JWT/Abstract/ITokenHelper.cs
@@ -0,0 +1,9 @@
+﻿using Northwind.Core.Entities.Concrete;
+using Northwind.Core.Utilities.Security.JWT.Concrete;
+
+namespace Northwind.Core.Utilities.Security.JWT.Abstract;
+
+public interface ITokenHelper
+{
+	AccessToken CreateToken(User user, List<OperationClaim> operationClaims);
+}
---
+++ Northwind.Core/Utilities/Security/JWT/Concrete/JwtHelper.cs
@@ -0,0 +1,12 @@
+﻿using Northwind.Core.Entities.Concrete;
+using Northwind.Core.Utilities.Security.JWT.Abstract;
+
+namespace Northwind.Core.Utilities.Security.JWT.Concrete;
+
+public class JwtHelper : ITokenHelper
+{
+	public AccessToken CreateToken(User user, List<OperationClaim> operationClaims)
+	{
+		throw new NotImplementedException();
+	}
+}
