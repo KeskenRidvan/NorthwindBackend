a0d49085da243c45665231882979284d0f1a6428
Ridvan Kesken <kesken.dev@gmail.com>
2024-01-04 20:01:26 +0300

    Created system that returns Result and DataResult.

---
+++ Northwind.Core/Utilities/Results/Concrete/DataResult.cs
@@ -0,0 +1,18 @@
+﻿using Northwind.Core.Utilities.Results.Abstract;
+
+namespace Northwind.Core.Utilities.Results.Concrete;
+
+public class DataResult<TEntity> : Result, IDataResult<TEntity>
+{
+
+	public DataResult(TEntity data, bool success, string message) : base(success, message)
+	{
+		Data = data;
+	}
+	public DataResult(TEntity data, bool success) : base(success)
+	{
+		Data = data;
+	}
+
+	public TEntity Data { get; }
+}
