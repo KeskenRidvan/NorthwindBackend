56e8db69bb2f4611d810b94d9791b118f6559559
Ridvan Kesken <kesken.dev@gmail.com>
2024-01-04 19:16:36 +0300

    Coded Context object.

---
+++ Northwind.DataAccess/Concrete/EntityFramework/Context/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Northwind.DataAccess.Abstract;
+
+namespace Northwind.DataAccess.Concrete.EntityFramework.Context;
+
+public class AppDbContext : DbContext
+{
+	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+	{
+		optionsBuilder.UseSqlServer(@"Server=(localdb)\UDEMY;Database=Eng.Northwind;Trusted_Connection=true");
+	}
+
+	public DbSet<Product> Products { get; set; }
+}
