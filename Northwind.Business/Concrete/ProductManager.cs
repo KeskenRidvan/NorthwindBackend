1a5a91e22982b3f3b937f955ce1a9477660ec9a4
Ridvan Kesken <kesken.dev@gmail.com>
2024-01-04 20:47:46 +0300

    Magic strings got rid of.

--- Northwind.Business/Concrete/ProductManager.cs
+++ Northwind.Business/Concrete/ProductManager.cs
@@ -1,4 +1,5 @@
 ﻿using Northwind.Business.Abstract;
+using Northwind.Business.Constants;
 using Northwind.Core.Utilities.Results.Abstract;
 using Northwind.Core.Utilities.Results.Concrete;
 using Northwind.DataAccess.Abstract;
@@ -18,33 +19,33 @@ public class ProductManager : IProductService
 	public IResult Add(Product product)
 	{
 		_productDal.Add(product);
-		return new SuccessResult("Ürün başarıyla eklendi.");
+		return new SuccessResult(Messages.ProductAdded);
 	}
 
 	public IResult Update(Product product)
 	{
 		_productDal.Update(product);
-		return new SuccessResult("Ürün başarıyla güncellendi.");
+		return new SuccessResult(Messages.ProductUpdated);
 	}
 
 	public IResult Delete(Product product)
 	{
 		_productDal.Delete(product);
-		return new SuccessResult("Ürün başarıyla silindi.");
+		return new SuccessResult(Messages.ProductDeleted);
 	}
 
 	public IDataResult<List<Product>> GetAll()
 	{
-		return new SuccessDataResult<List<Product>>(_productDal.GetList().ToList());
+		return new SuccessDataResult<List<Product>>(_productDal.GetList().ToList(), Messages.ProductsListed);
 	}
 
 	public IDataResult<Product> GetById(int productId)
 	{
-		return new SuccessDataResult<Product>(_productDal.Get(p => p.ProductId.Equals(productId)));
+		return new SuccessDataResult<Product>(_productDal.Get(p => p.ProductId.Equals(productId)), Messages.ProductListed);
 	}
 
 	public IDataResult<List<Product>> GetListByCategory(int categoryId)
 	{
-		return new SuccessDataResult<List<Product>>(_productDal.GetList(c => c.CategoryId.Equals(categoryId)).ToList());
+		return new SuccessDataResult<List<Product>>(_productDal.GetList(c => c.CategoryId.Equals(categoryId)).ToList(), Messages.ProductsListedByCategory);
 	}
 }
---
+++ Northwind.Business/Constants/Messages.cs
@@ -0,0 +1,11 @@
+﻿namespace Northwind.Business.Constants;
+
+public static class Messages
+{
+	public static string ProductAdded = "Ürün başarıyla eklendi!";
+	public static string ProductUpdated = "Ürün başarıyla eklendi!";
+	public static string ProductDeleted = "Ürün başarıyla silindi!";
+	public static string ProductsListed = "Ürünler listelendi!";
+	public static string ProductListed = "Ürün listelendi!";
+	public static string ProductsListedByCategory = "Ürünler kategoriye göre listelendi!";
+}
