b8ce1501fab4cb4a025ab66890346b2700601b5e
Ridvan Kesken <kesken.dev@gmail.com>
2024-01-04 22:51:08 +0300

    SecurityKeyHelper was written.

--- Northwind.Core/Utilities/Security/JWT/Concrete/JwtHelper.cs
+++ Northwind.Core/Utilities/Security/JWT/Concrete/JwtHelper.cs
@@ -1,12 +1,24 @@
-﻿using Northwind.Core.Entities.Concrete;
+﻿using Microsoft.Extensions.Configuration;
+using Northwind.Core.Entities.Concrete;
+using Northwind.Core.Utilities.Security.Encyption;
 using Northwind.Core.Utilities.Security.JWT.Abstract;
 
 namespace Northwind.Core.Utilities.Security.JWT.Concrete;
 
 public class JwtHelper : ITokenHelper
 {
+	public IConfiguration Configuration { get; }
+	private readonly TokenOptions _tokenOptions;
+
+	public JwtHelper(IConfiguration configuration)
+	{
+		Configuration = configuration;
+		_tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>(); // Paketlerden Microsoft.Extensions.Configuration.Binder kurulması gerekiyor.
+	}
+
 	public AccessToken CreateToken(User user, List<OperationClaim> operationClaims)
 	{
-		throw new NotImplementedException();
+
+		var securityKey = SecurityKeyHelper.CreateSecurityKey(_tokenOptions.SecurityKey);
 	}
 }
