15533866152dba69fa776399d966c9ebb829eb2d
Ridvan Kesken <kesken.dev@gmail.com>
2024-01-04 19:47:03 +0300

    Start coding the Business layer.

---
+++ Northwind.Business/Abstract/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using Northwind.Entities.Concrete;
+
+namespace Northwind.Business.Abstract;
+
+public interface IProductService
+{
+	Product GetById(int productId);
+	List<Product> GetAll();
+	List<Product> GetListByCategory(int categoryId);
+	void Add(Product product);
+	void Update(Product product);
+	void Delete(Product product);
+}
