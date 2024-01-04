2a121eeea3a4017cb0165b75686c853dea26f00d
Ridvan Kesken <kesken.dev@gmail.com>
2024-01-04 22:34:52 +0300

    Dto implemantation was provided.

---
+++ Northwind.Entities/DTOs/UserForLoginDto.cs
@@ -0,0 +1,9 @@
+﻿using Northwind.Core.Entities.Abstract;
+
+namespace Northwind.Entities.DTOs;
+
+public class UserForLoginDto : IDto
+{
+	public string Email { get; set; }
+	public string Password { get; set; }
+}
