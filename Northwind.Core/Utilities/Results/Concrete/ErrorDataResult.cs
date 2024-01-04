ec2f778721a5fc8842cb3cff7a4cf215272814dc
Ridvan Kesken <kesken.dev@gmail.com>
2024-01-04 20:06:50 +0300

    ErrorDataResult infrastructure was established.

---
+++ Northwind.Core/Utilities/Results/Concrete/ErrorDataResult.cs
@@ -0,0 +1,11 @@
+﻿namespace Northwind.Core.Utilities.Results.Concrete;
+
+public class ErrorDataResult<TEntity> : DataResult<TEntity>
+{
+	public ErrorDataResult(TEntity data, string message) : base(data, false, message) { }
+
+	public ErrorDataResult(TEntity data) : base(data, false) { }
+
+	public ErrorDataResult(string message) : base(default, false, message) { }
+	public ErrorDataResult() : base(default, false) { }
+}
