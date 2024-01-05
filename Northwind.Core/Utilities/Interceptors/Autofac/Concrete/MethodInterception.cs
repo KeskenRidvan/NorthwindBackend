c99e7e1269da8f9b713dc7a6813094aa4f2c84c4
Ridvan Kesken <kesken.dev@gmail.com>
2024-01-05 22:43:52 +0300

    Method interception infrastructure was established.

---
+++ Northwind.Core/Utilities/Interceptors/Autofac/Concrete/MethodInterception.cs
@@ -0,0 +1,34 @@
+﻿using Castle.DynamicProxy;
+
+namespace Northwind.Core.Utilities.Interceptors.Autofac.Concrete;
+
+public class MethodInterception : MethodInterceptionBaseAttribute
+{
+	protected virtual void OnBefore(IInvocation ınvocation) { }
+	protected virtual void OnAfter(IInvocation ınvocation) { }
+	protected virtual void OnException(IInvocation ınvocation) { }
+	protected virtual void OnSuccess(IInvocation ınvocation) { }
+
+	public override void Intercept(IInvocation invocation)
+	{
+		var isSuccess = true;
+		OnBefore(invocation);
+
+		try
+		{
+			invocation.Proceed();
+		}
+		catch (Exception ex)
+		{
+			isSuccess = false;
+			OnException(invocation);
+			throw;
+		}
+		finally
+		{
+			if (isSuccess)
+				OnSuccess(invocation);
+		}
+		OnAfter(invocation);
+	}
+}
