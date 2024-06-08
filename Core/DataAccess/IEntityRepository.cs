71839eebd3d90de86e2d7095716b1d384ba7dd62
Ridvan Kesken <kesken.dev@gmail.com>
2024-06-08 13:55:53 +0300

    The data access layer is coded. - Repository pattern coded. - Context object coded. - Repository base object coded.

---
+++ Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,13 @@
+﻿using Core.Entities;
+using System.Linq.Expressions;
+
+namespace Core.DataAccess;
+public interface IEntityRepository<TEntity>
+	where TEntity : class, IEntity, new()
+{
+	TEntity Get(Expression<Func<TEntity, bool>> filter);
+	IList<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null);
+	void Add(TEntity entity);
+	void Update(TEntity entity);
+	void Delete(TEntity entity);
+}
---
+++ DataAccess/Concrete/EntityFramework/Context/NorhwindContext.cs
@@ -0,0 +1,14 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concrete.EntityFramework.Context;
+public class NorhwindContext : DbContext
+{
+	protected override void OnConfiguring(
+		DbContextOptionsBuilder optionsBuilder)
+	{
+		optionsBuilder.UseSqlServer(@"Server=(localdb)\UDEMY;Database=Eng.Northwind;Trusted_Connection=true");
+	}
+
+	public DbSet<Product> Products { get; set; }
+}
